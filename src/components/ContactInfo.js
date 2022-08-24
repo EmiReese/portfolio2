702ac76755b977e9bb74c144b183dc0300e24862
Emily Reese <emilyreese.ga@gmail.com>
2022-08-23 20:48:43 -0400

    I changed the font,background color and redid my contact information component.

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,52 +1,102 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';  
-import {Container , Row, Col} from 'react-bootstrap'
-import { PhoneFill, EnvelopeFill, Github, Linkedin,   } from 'react-bootstrap-icons';
+
 
 export default function Contact() {
 return (
-  
-<div className='fs-4' >
-  <Container fluid>
-   <Row className="p-3 offset-1">
-   <h3 id="Contact" className= "mb-5">Contact Information</h3>
-   </Row>
-    <Row className="p-3 offset-1">
-      <Col xs = {12} lg ={5}>
-      <div className="ms-2 me-auto">
-      <div className="fw">Phone Number <PhoneFill></PhoneFill></div>
-      678 978 9459
+
+    <section class="mb-4">
+        <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
+
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me directly.</p>
+
+    <div class="row">
+
+      <div class="col-md-9 mb-md-0 mb-5">
+         <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+          
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="name" name="name" class="form-control" />
+                        <label for="name" class="">Your name</label>
+                    </div>
+                </div>
+           
+           
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="email" name="email" class="form-control"/>
+                        <label for="email" class="">Your email</label>
+                    </div>
+                </div>
+              
+
+            </div>
+         
+
+      
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form mb-0">
+                        <input type="text" id="subject" name="subject" class="form-control"/>
+                        <label for="subject" class="">Subject</label>
+                    </div>
+                </div>
+            </div>
+         
+            <div class="row">
+
+
+                <div class="col-md-12">
+
+                    <div class="md-form">
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                        <label for="message">Your message</label>
+                    </div>
+
+                </div>
+            </div>
+           
+
+        </form>
+
+        <div class="text-center text-md-left">
+            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+        </div>
+        <div class="status"></div>
     </div>
-     </Col>
 
-     <Col xs = {12} lg ={6}>
-    <div className="ms-2 me-auto">
-      <div className="fw">Email <EnvelopeFill></EnvelopeFill></div>
-        emily_reese@yahoo.com
+    <div class="col-md-3 text-center">
+        <ul class="list-unstyled mb-0">
+            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                <p></p>
+            </li>
+
+            <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                <p>(678) 978-9459</p>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <p>emilyreese.ga@gmail.com</p>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <a href="https://www.linkedin.com/in/emily-reese-50546621b/">LinkedIn Profile</a>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <a href="https://github.com/EmiReese">Github Profile</a>
+            </li>
+        </ul>
     </div>
-    </Col>
-    </Row>
+ 
 
-<Row className="p-3 offset-1">
-    <Col xs = {12} lg ={5}>
-    <div className="ms-2 me-auto">
-      <div className="fw">    <a href="https://github.com/EmiReese">Github</a><br></br><Github>
-      </Github></div>
-    </div>
-  </Col>
-
-  <Col xs = {12} lg ={6}>
-    <div className="ms-2 me-auto">
-      <div className="fw"><a href="https://www.linkedin.com/in/emily-reese-50546621b/">LinkedIn</a><br></br>
-      <Linkedin>
-      </Linkedin>
-      </div>
-    </div>
-  </Col>
-</Row>
-
-
-</Container>
 </div>
+
+</section>
+
 );
 }
