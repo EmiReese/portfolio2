e7b2e65ee0f985b789e0d2652b76a6e5a67b9290
Emily Reese <emilyreese.ga@gmail.com>
2022-11-05 17:50:04 -0400

    I added a project that shows my fundamental knowledge of HTML, CSS, and Javascript.

--- src/components/carPortfolio.js
+++ src/components/carPortfolio.js
@@ -3,6 +3,7 @@ import '../stylesheets/carPortfolio.css'
 import VirtualPets from '../images/VirtualPets.png'
 import HC from '../images/HC.jpg'
 import CL from '../images/CL.png'
+import AS from '../images/albumSearch.png'
 
 function DarkVariantExample() {
   return (
@@ -17,20 +18,21 @@ function DarkVariantExample() {
         <Carousel.Caption className='carouselCaption h-100 w-50'>
           <h5>Virtual Pets</h5>
           <p>Virtual Pets is a web-based app that allows for pet parents to make telehealth consultation appointments with Veterinarians. </p>
-        <a href="https://github.com/CSanchez486/Virtual_Pet" className="portfolioCodeLink">The code</a>
+        <a href="https://virtualpets1234.herokuapp.com/" className="portfolioCodeLink">The Deployment</a>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className="h-100 w-100 carouselItem">
         <img
-          src={HC}
+          src={AS}
           alt="A notebook and list"
         />
         <Carousel.Caption className='carouselCaption  h-100 w-50'>
      
-          <h5>HandyCart</h5>
-          <p>This is a retail purchasing application leveraging contact list technology and financial budget parameters.</p>
-          <a href="https://github.com/EmiReese/HandyCart" className="portfolioCodeLink">The code</a>
+          <h5>Album Search</h5>
+       
+          <p>This is an album searching application. Created as an example of my fundamental knowledge of HTML, CSS and Javascript.</p>
+          <a href="https://emireese.github.io/Album-Search/" className="portfolioCodeLink">The Deployment</a>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="h-100 w-100 carouselItem">
@@ -44,7 +46,7 @@ function DarkVariantExample() {
           <p>
           An application intended to help underprivileged people or families connect to non-profit organizations closest to their location with resources like food.
           </p>
-        <a href="https://github.com/Cheez0id/Charity-Locator-Project-P1G7" className="portfolioCodeLink">The code</a>
+        <a href="https://cheez0id.github.io/Charity-Locator-Project-P1G7/" className="portfolioCodeLink">The Deployment</a>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
